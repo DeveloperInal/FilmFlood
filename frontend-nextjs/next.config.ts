2bb5514609196a406c71d3fceedbe94feb5f6486
inaldev <gergovinal770@gmail.com>
2024-12-21 06:13:45 +0300

    Update next.config.ts

--- frontend-nextjs/next.config.ts
+++ frontend-nextjs/next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  webpack: (config) => {
+    // Проверка на конфликты с alias
+    config.resolve.alias = { 
+      'next/dynamic': require.resolve('next/dynamic'),
+    }},
   pageExtensions: ['mdx', 'md', 'jsx', 'js', 'tsx', 'ts'],
 
   images: {
