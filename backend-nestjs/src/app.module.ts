25f2bca9679c61e0634f2a73f277ac3bdbaf17bd
inaldev <gergovinal770@gmail.com>
2025-02-04 23:54:18 +0300

    upgrade

--- backend-nestjs/src/app.module.ts
+++ backend-nestjs/src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import {CacheModule} from "@nestjs/cache-manager";
 import { PrismaService } from './prisma.service';
 import { ConfigModule } from '@nestjs/config';
 import { FilmResModule } from './film_res/film_res.module';
@@ -20,10 +19,6 @@ import { UserModule } from './user/user.module';
 @Module({
   imports: [ConfigModule.forRoot({
     isGlobal: true
-  }), CacheModule.register({
-    isGlobal: true,
-    host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT,
   }), FilmResModule, AuthModule, EmailModule, S3Module, UserModule],
   controllers: [FilmResController, S3Controller],
   providers: [PrismaService, FilmResService, AuthService, NestJwtService, EmailService, JwtService, RedisService, S3Service],
