1013d1452a28b323f02ff133fd05f739e1f3216c
inaldev <gergovinal770@gmail.com>
2025-02-05 21:25:55 +0300

    upgrade

--- backend-nestjs/src/auth/email/email.service.ts
+++ backend-nestjs/src/auth/email/email.service.ts
@@ -1,17 +1,20 @@
 // EmailVerification
 import { Injectable } from '@nestjs/common';
 import axios from 'axios';
+import * as process from "node:process";
 
 @Injectable()
 export class EmailService {
-    async sendEmail(email: string) { // Функция отправки кода на почту
-        const sendUrl = 'http://fastapi:8000/api/v1/send_email?email=';
+    async sendEmail(email: string) {
+        const url = process.env.FASTAPI_URL// Функция отправки кода на почту
+        const sendUrl = `${url}/api/v1/send_email?email=`;
         const email_post = await axios.get(`${sendUrl}${email}`);
         return email_post.data;
     }
 
-    async compration_code(code: number): Promise<boolean> { // Функия проверки кода
-        const comprationUrl = 'http://fastapi:8000/api/v1/verify_code?code=';
+    async compration_code(code: number): Promise<boolean> {
+        const url = process.env.FASTAPI_URL// Функция отправки кода на почту// Функия проверки кода
+        const comprationUrl = `${url}/api/v1/verify_code?code=`;
         const response = await axios.get(`${comprationUrl}${code}`);
         return response.data === true;
     }
